cf36c12bc68bb03328bdc25a769134c3d4df7018
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-04-09 14:51:16 +0900

    Add Closure

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         //처음 스위치 상태를 거짓으로 설정
     }
     
-    @objc func updateTime() {
+    func updateTime() {
         let date = Date()//현재 시스템 시간,날짜등
         let formatter = DateFormatter() //출력할 형태 지정
         formatter.dateFormat = "a HH:mm:ss EEE" // 시간:분:초 날짜(EEE : 영어 앞에 3글자, EEEE : 영어 전체, EEEEE : 영어 첫자, a : am,pm 출력)
@@ -33,7 +33,13 @@ class ViewController: UIViewController {
     @IBAction func timeswitch(_ sender: Any) {
         if timeSwitch.isOn == true //스위치가 참이면 타이머 실행
         {
-            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+//            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()// Closure를 이용해서 함수를 인자로 넘긴다.
+            })
+            
+            
         }
         else//거짓이면 타이머 종료
         {myTimer.invalidate()
